4728a6cde36bfb648ac400805fce55430856f980
wizzycliffy <trymailwizzy@gmail.com>
2017-08-21 21:22:28 +0100

    Create warriboys.js

---
+++ docs/warriboys.js
@@ -0,0 +1,36 @@
+/***
+warri boys in action..lol
+***/
+	 function downloadLink(){
+	 location.assign("http://google.com");
+	 }  
+	
+	 function ask(){
+	 do { 
+	 alert("You must enter a name to proceed, click OK to enter your name");
+	 var name = prompt("Please enter your name");
+	 }
+	 while (name === null ||
+	     name === " " || 
+	     name === "" );
+	if (name === "Franklin Mba"){
+	    if (confirm("Our dear son " + name + " We're your Original VILLAGERS, we have finally catch up with you online today. hahaha! If you're ready to settle us now click OK") === true) {  
+	  var amount = prompt("Oya, Enter the amount 20million USD here. Remember, we're using an updated Shongo version so we only accept bitcoin or USD.");
+	 checkAmount(amount);
+}else{
+	    alert("We will not free your matter");
+	    location.reload();
+	    }
+}else {
+	 alert("Oga, we don't know you. Who you help? ");
+	 location.reload();
+	 	 }
+}
+function checkAmount(amount){
+	 if (amount == 20){
+	    alert("Good son! We have  freed your matter, you can now download your flier");
+	     downloadLink();
+    }else { alert("invalid amount, only 20million USD can settle us!"); 
+	    location.reload(); 
+	    }
+	 } 
